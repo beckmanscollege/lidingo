e2566856c137f0bd4875f077afc8de30540cd9fd
Jonas Johansson <jns.johansson@gmail.com>
2024-11-29 21:30:31 +0100

    x

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const projects = [
         {
-            title: "Astrea Kulturhus",
             gid: 1,
+            title: "Astrea Kulturhus",
+            lon: 18.139,
+            lat: 59.366,
             flip: true,
             html: `
                     <p>Astrea kulturhus byggs ovanpå en stor parkeringsplats mitt i centrum, där hantverk, kreativitet och möten är i fokus. Här finns verkstäder med specialutrustning för keramik, screentryck och musikstudio, samt ett café och utrymme för evenemang som marknader och spelningar.</p>
@@ -22,8 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Optiktornet MinusEtt",
             gid: 2,
+            title: "Optiktornet MinusEtt",
+            lon: 18.1556,
+            lat: 59.3472,
             html: `
                     <p>Optiktornet MinusEtt är en mötesplats för Lidingös konst- och designskolor i det övergivna Optiktornet och bergrummet i AGA. Här kan studenter och allmänhet samlas för utställningar och evenemang, vilket ger Lidingö en ny, spännande plats som lockar en ung, kreativ målgrupp.</p>
                     <p>Lidingö har länge varit känt som en exklusiv, anrik och naturnära plats för familjer och äldre, men saknar en tydlig mötesplats för yngre konstnärer och designers. Trots att flera konst- och designskolor finns på ön, är kontakten mellan studenterna bristfällig. Vi vill därför skapa en gemensam plats där de kan samlas, knyta kontakter och visa upp sina projekt.</p>
@@ -33,13 +37,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     <img src="assets/2/optiktornetminusett3.jpg" />
                     <img src="assets/2/optiktornetminusett4.jpg" />
                     <img src="assets/2/optiktornetminusett5.jpg" />
+                    <img src="assets/2/optiktornetminusett6.jpg" />
+                    <img src="assets/2/optiktornetminusett7.jpg" />
                     <h3>Arbetsgrupp</h3>
                     <p>Lina Johansson, Olle Ljung, Edda Eliasson</p>
                 `,
         },
         {
-            title: "Viveka",
+
             gid: 3,
+            title: "Viveka",
+            lon: 18.114561,
+            lat: 59.383096,
             html: `
                     <p>På gränsens mellan norra och södra Sticklinge på Lidingö lanseras ett projekt för att antingen stärka grannskapets gemenskap - eller belysa en bristande sammanhållning i området.</p>
                     <p>"Ryktet om Lidingöborna som självupptagna översittare har orsakat både osämja och dålig självbild bland Lidingöborna själva, och för att öns rykte ska kunna räddas så måste det ske en förändring inifrån och ut.</p>
@@ -53,8 +62,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Tindra",
             gid: 4,
+            title: "Tindra",
+            lon: 18.135770,
+            lat: 59.364163,
             html: `
                     <p>I det övergivna och nedklottrade Tindrahusets plats ska vi upprätta Tindra graffitipark. Parken ska vara ett levande galleri i konstant förändring, som hyllar Tindrahusets nuvarande roll som canvas för klotter. Projektet innefattar även en informationsskylt samt en hemsida där graffitin dokumenteras.</p>
                     <p>Tindrahuset är i mycket dåligt skick, anses vara en otrygg plats och ska rivas – men samtidigt är det kulturellt värdefullt. Graffitiparken hyllar husets historia och agerar som en alternativ mötesplats, annorlunda från Lidingös villaområden och fotbollsplaner.</p>
@@ -67,8 +78,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Gåshaxa x Elfvik",
             gid: 5,
+            title: "Gåshaxa x Elfvik",
+            lon: 18.228281,
+            lat: 59.362983,
             html: `
                     <p>Lidingö är rikt på natur och sevärdheter, dessa ligger dock långt ifrån varandra och kräver bil för att ta sig emellan. Detta projekt syftar till att knyta ihop sundet mellan Gåshaga och  Elfvik, såväl geografiskt som socialt. Med inspiration av skärgården och närliggande bebyggelse har vi formgivit en ögrupp med broar som aktiverar och leder besökarna över sundet.</p>
                     <h3>Arbetsgrupp</h3>
@@ -76,8 +89,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Fountain",
             gid: 6,
+            title: "Fountain",
+            lon: 18.13349,
+            lat: 59.36431,
             html: `
                     <p>Utifrån stadshusets nuvarande arkitektur arbetar vi med en lösning som passar in utan att ta över. Den nya fontänen ska bli en naturlig del av platsen, samtidigt som den tillför något nytt och gör stadshuset mer relevant och inbjudande för människorna som vistas i området.</p>
                     <p>Hur förändrar man uppfattningen om en byggnad utan att riva den? Många Lidingöbor upplever att stadshusets utseende inte passar in i den idylliska miljö de förknippar med ön, och vi ser istället möjligheten att förbättra platsen genom att lyfta fram dess omgivning.</p>
@@ -96,8 +111,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Ett spår av konst",
             gid: 7,
+            title: "Ett spår av konst",
+            lon: 18.179641,
+            lat: 59.344519,
             html: `
                     <p>Med utgångspunkt från Lidingös historia och aktiviteter vill vi lysa upp varje station längst med lidingöbanan med konst. </p>
                     <p>Tunnelbanan är världens längsta konstutställning som lockar många turister och besökare, konsten är väldigt uppskattad av de som åker tunnelbanan och bidrar med kulturella intryck. Motivationen till tunnelbanans konst var att alla har rätt till att se konst och det tycker vi lidingöborna också har. Genom att skapa konst längst med lidingöbanan så kanske man får folk att vilja åka ut till Lidingö för att se på den, och världens längsta konstustställning blir ännu längre. Vi tror även att de som bor på Lidingö och åker lidingöbanan varje dag kommer uppskatta något som lyser upp och bryter av från omgivningen.</p>
@@ -118,8 +135,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 `,
         },
         {
-            title: "Vindarnas Tempel",
             gid: 8,
+            title: "Vindarnas Tempel",
+            lon: 18.0642,
+            lat: 59.2149,
             html: `
                     <p>Vårt koncept är en bar och en social mötesplats vars utformning och funktion för samman platsens historiska kontext med dess nuvarande kontext. Temat rörelse har lett oss till att skapa en cykelbar med tillhörande barbord, för att bjuda in folk i rörelse att stanna upp för en kort stund.</p>
                     <p>Platsen är i dagsläget ett "mellanområde", den bjuder halvt in cyklister och fotgängare (bron är enbart till för cykel/gång) men ingen stannar faktiskt där, då platsen saknar funktion. Med baren och de nya cykelborden på plats så finns det en anledning och möjlighet för folk att enkelt stanna på vägen till något annat, eller ha platsen som en destination i sig.</p>
@@ -187,6 +206,10 @@ document.addEventListener("DOMContentLoaded", () => {
         grid.appendChild(cell);
     });
 
+    function generateGoogleMapsLink(longitude, latitude) {
+        return `https://www.google.com/maps?q=${latitude},${longitude}&z=15`;
+    }
+
     // Show fullscreen on cell click
     grid.addEventListener("click", (e) => {
         const cell = e.target.closest(".cell");
@@ -196,7 +219,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             fullscreen.style.display = "flex";
             fullscreenTitle.textContent = project.title || "";
-            fullscreenContent.innerHTML = project.html || "";
+            fullscreenContent.innerHTML = `<a href="${generateGoogleMapsLink(project.lon, project.lat)}" target="_blank">${project.lon}, ${project.lat}</a>`
+            fullscreenContent.innerHTML += project.html || "";
 
             // Replace the model-viewer in the fullscreen model container
             modelContainer.innerHTML = "";
